274a0188adae10f911f6afe4bd3f3787e072a546
Miguel.Angelo <migsilva95@gmail.com>
2023-01-17 16:12:52 -0300

    adicionada api usando axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [indicadores, setIndicadores] = useState(null)
+
+  useEffect(() => {
+    startAPI()
+    console.log('i fire once');
+   }, [])
+
+  function startAPI() {
+    axios
+      .get("http://localhost:3000/indicadores")
+      .then(response => {
+        console.log(response.data);
+        setIndicadores(response.data)
+      })
+      // if error
+      .catch(function(error) {
+        console.log(error);
+      });
+
+      axios
+      .get("http://localhost:3000/simulacoes")
+      .then(response => {
+        var simulacoes = response.data;
+        console.log(simulacoes);
+      })
+      // if error
+      .catch(function(error) {
+        console.log(error);
+      });
+
+  }
+
+    return (
+      <div>
+        <button type="submit" label="Send" onClick={(() => console.log("ws"))}>
+          Send!
+        </button>
+        {indicadores ? indicadores[0].nome : null}
+      </div>
+    );
+
+}
 export default App;
